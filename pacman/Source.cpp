f757b4e387b289c9d37454048b3a4ca670482c79
Valen <Valen@DESKTOP-LBI3N59>
2017-01-12 21:00:50 +0100

    .txt Done

--- pacman/Source.cpp
+++ pacman/Source.cpp
@@ -4,9 +4,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
+
 #pragma comment (lib, "Ws2_32.lib")
+
+struct Player {
+	int score;
+	std::string name;
+};
+
+void PrintRanking(Player ranking[]) {
+	for (int i = 0; i < 10; i++) {
+		cout << i+1 << " - Name: " << ranking[i].name;
+		cout << " Score: " << ranking[i].score << endl;
+	}
+}
+
+void OrdenarArray(Player ranking[]) {
+	int auxScore;
+	std::string auxName;
+	for (int a = 0; a < 30; a++) {
+		for (int i = 0; i < 10; i++) {
+			if (ranking[i].score < ranking[i + 1].score) {
+				auxScore = ranking[i + 1].score;
+				auxName = ranking[i + 1].name;
+				ranking[i + 1].score = ranking[i].score;
+				ranking[i + 1].name = ranking[i].name;
+				ranking[i].score = auxScore;
+				ranking[i].name = auxName;
+			}
+		}
+	}
+}
+
+void ReadTextFile() {
+	Player ranking[10];
+	std::string lectura;
+	ifstream fEntrada;
+	fEntrada.open("ranking.txt", ios::in);
+	if (fEntrada.fail()) {
+		cout << "<WARNING> File can not be opened" << endl;
+	}
+	else {
+		bool state = false;
+		int i = 0;
+		for(int a = 0; a < 20; a++) {
+			getline(fEntrada, lectura);
+			if (state == false) {
+				ranking[i].name = lectura;
+				state = true;
+			}
+			else if (state == true) {
+				ranking[i].score = std::stoi(lectura,nullptr,10);
+				state = false;
+				i++;
+			}
+		}
+	}
+	fEntrada.close();
+	OrdenarArray(ranking);
+	PrintRanking(ranking);
+}
+
+
+
+void WriteTextFile() {
+	Player ranking[10];
+	for (int i = 0; i < 10; i++) {
+		ranking[i].name = "Test";
+		ranking[i].score = 1234;
+	}
+	ofstream fSalida;
+	fSalida.open("ranking.txt", ios::out);
+	if (fSalida.fail()) {
+		cout << "<WARNING> File can not be opened" << endl;
+	}
+	else {
+		for (int i = 0; i < 10; i++) {
+			fSalida << ranking[i].name << "\n" << ranking[i].score << endl;
+		}
+	}
+	fSalida.close();
+}
+
 void server() {
+	ReadTextFile();
+	//WriteTextFile();
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -39,16 +125,19 @@ void server() {
 	bufer[i - 1] = '\0';
 	if (recv) {
 		cout << "INFO: Recv correct" << endl;
-		cout << bufer << endl;
+		//cout << bufer << endl;
 	}
 	else {
 		cout << "INFO: Recv wrong" << endl;
 	}
-	//cout << bufer << endl;
+	
+	
+
 	shutdown(socKrec, SD_RECEIVE);
 	closesocket(socKrec);
 	WSACleanup();
 }
+
 int main(int argc, char **argv) { //port 5219
 	for (;;) {
 		server();
